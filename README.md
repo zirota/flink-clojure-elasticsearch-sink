f02e0aefe7bbec22399894b1d9c42b93bb49ab0b
Hazzen Chua <hazzenchua@gmail.com>
2022-08-14 23:05:30 +0800

    updated README & project settings

--- README.md
+++ README.md
@@ -4,9 +4,25 @@ A Clojure wrapper for the Elasticsearch 7 Flink Connector
 
 ## Usage
 
-Simply import the library
+Add the library to your dependencies.
 
-## Examples
+### Leiningen
+```
+[org.clojars.hazzenc/flink-clojure-elasticsearch-sink "0.1.0-SNAPSHOT"]
+```
+
+### Clojure CLI / deps.edn
+```
+org.clojars.hazzenc/flink-clojure-elasticsearch-sink {:mvn/version "0.1.0-SNAPSHOT"}
+```
+### Maven
+```
+<dependency>
+  <groupId>org.clojars.hazzenc</groupId>
+  <artifactId>flink-clojure-elasticsearch-sink</artifactId>
+  <version>0.1.0-SNAPSHOT</version>
+</dependency>
+```
 
 
 ### Author
--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
 
 (defproject org.clojars.hazzenc/flink-clojure-elasticsearch-sink "0.1.0-SNAPSHOT"
   :description "A Clojure wrapper for the Elasticsearch 7 Flink Connector"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/zirota/flink-clojure-elasticsearch-sink"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :global-vars {*warn-on-reflection* true}
@@ -11,6 +11,10 @@
                  [org.clojure/data.json "2.4.0"]
                  [org.apache.flink/flink-connector-elasticsearch7 ~(:flink version)]
                  [org.apache.flink/flink-streaming-java ~(:flink version) :scope "provided"]]
+  :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
+                                    :username      :env/clojars_username
+                                    :password      :env/clojars_password
+                                    :sign-releases false}]]
   :main ^:skip-aot flink-clojure-elasticsearch-sink.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
